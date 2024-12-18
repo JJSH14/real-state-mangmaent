9f17891ff367c55852a43f9902d1ae50fe3be073
Nora <s12217202@stu.najah.edu>
2024-12-18 18:42:19 +0200

    add some ui changes

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,11 +1,11 @@
 module com.example.loborems {
-    requires javafx.controls;
     requires javafx.fxml;
 
     requires org.controlsfx.controls;
     requires com.dlsc.formsfx;
     requires net.synedra.validatorfx;
     requires org.kordamp.bootstrapfx.core;
+    requires com.jfoenix;
 
     opens com.example.loborems to javafx.fxml;
     exports com.example.loborems;
