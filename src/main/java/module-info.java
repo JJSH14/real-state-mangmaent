82452a164cdc0875acda923a7acaab3ddb2b3120
Nora <s12217202@stu.najah.edu>
2024-12-18 18:45:36 +0200

    undo

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,7 +5,6 @@ module com.example.loborems {
     requires com.dlsc.formsfx;
     requires net.synedra.validatorfx;
     requires org.kordamp.bootstrapfx.core;
-    requires com.jfoenix;
 
     opens com.example.loborems to javafx.fxml;
     exports com.example.loborems;
