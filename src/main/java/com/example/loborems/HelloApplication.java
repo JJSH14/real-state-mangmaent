29b80a11e4f663a4068ccb655e72bf2d695cc92c
Jihade Shtayyeh <152526397+JJSH14@users.noreply.github.com>
2024-12-26 23:31:38 +0200

    Update HelloApplication.java

--- src/main/java/com/example/loborems/HelloApplication.java
+++ src/main/java/com/example/loborems/HelloApplication.java
@@ -7,12 +7,13 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
+
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("Login/login.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("addAgent.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
-        stage.setTitle("Lobo Real Estate");
+        stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
     }
