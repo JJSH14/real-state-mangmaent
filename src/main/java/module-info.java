cea1bab558d818888ede20b2ad9491d029ae921e
Jihade Shtayyeh <152526397+JJSH14@users.noreply.github.com>
2024-12-26 23:34:27 +0200

    Update module-info.java

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,31 +1,22 @@
 module com.example.loborems {
-    requires javafx.controls;
     requires javafx.fxml;
 
     requires org.controlsfx.controls;
     requires com.dlsc.formsfx;
     requires net.synedra.validatorfx;
     requires org.kordamp.bootstrapfx.core;
-    requires java.logging;
     requires org.hibernate.orm.core;
-    requires java.naming;
+    requires java.persistence;
     requires java.sql;
     requires mysql.connector.java;
-    requires java.persistence;
-    requires java.desktop;
+    requires java.naming;
+    requires jbcrypt;
+    requires org.slf4j;
+    requires com.jfoenix;
 
-
-    // Export and open necessary packages
-
-
-    opens com.example.loborems to javafx.fxml;
+    opens com.example.loborems;
+    opens com.example.loborems.models;
     exports com.example.loborems;
-    exports com.example.loborems.controllers;
-    exports com.example.loborems.interfaces;
-    opens com.example.loborems.interfaces to javafx.fxml;
-    opens com.example.loborems.models to javafx.fxml, org.hibernate.orm.core;  // Open to both javafx.fxml and Hibernate;
-    exports com.example.loborems.models;
-    opens com.example.loborems.util to javafx.fxml;
-    exports com.example.loborems.util;
-    opens com.example.loborems.controllers to javafx.fxml;
+    exports com.example.loborems.controller.authntociation;
+    opens com.example.loborems.controller.authntociation to javafx.fxml;
 }
