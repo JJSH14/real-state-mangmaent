7471a8c0fe562a284830a602c32b340362689c3f
Abdul-Kareem Zaid <s12113676@stu.najah.edu>
2024-12-27 20:58:16 +0200

    run hibernate at start

--- src/main/java/com/example/loborems/HelloApplication.java
+++ src/main/java/com/example/loborems/HelloApplication.java
@@ -7,9 +7,12 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
+import com.example.loborems.util.HibernateUtil;
+
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
+         HibernateUtil.getSessionFactory();
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("Login/login.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("Lobo Real Estate");
