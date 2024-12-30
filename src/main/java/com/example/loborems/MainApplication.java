ec6cffa72da8ab38e655a14a967dcf3ec1980f21
Abdul-Kareem Zaid <s12113676@stu.najah.edu>
2024-12-30 23:13:57 +0200

    rename HelloAPp to a pROPER NAME FINALLy

--- src/main/java/com/example/loborems/MainApplication.java
+++ src/main/java/com/example/loborems/MainApplication.java
@@ -7,10 +7,10 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-public class HelloApplication extends Application {
+public class MainApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("Dashboard/dashboard.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("Dashboard/dashboard.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("Lobo Real Estate");
         stage.setScene(scene);
